2372bfdf8330d7b7eca4dc42f60573414391c90b
Sebas Navarro <miguelsebanavarro@gmail.com>
2020-06-15 17:47:28 -0300

    Fix typo in bash script.

--- DockerRunImage.sh
+++ DockerRunImage.sh
@@ -4,4 +4,4 @@ FOLDER_NAME=$(basename $(pwd))
 CONTAINER_NAME=$(echo "$FOLDER_NAME" |  tr '[:upper:]' '[:lower:]' )
 
 echo "Running image..."$CONTAINER_NAME
-$ docker run $CONTAINER_NAME
+docker run $CONTAINER_NAME
